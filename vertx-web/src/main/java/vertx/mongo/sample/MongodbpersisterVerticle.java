efeed7b60f5669024b57f5f4696a0e1013f0ef40
julyinsung <julyinsung@gmail.com>
2017-06-14 18:24:50 +0900

    vertx web, mongo-client, eventBus 사용한 예제

---
+++ vertx-web/src/main/java/vertx/mongo/sample/MongodbpersisterVerticle.java
@@ -0,0 +1,71 @@
+package vertx.mongo.sample;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+
+/**
+ * http://vertx.io/docs/vertx-mongo-client/java/
+ * 
+ * @author july
+ *
+ */
+public class MongodbpersisterVerticle extends AbstractVerticle {
+
+	MongoClient mongoClient = null;
+
+	@Override
+	public void start() {
+		JsonObject config = new JsonObject();
+		config.put("host", "localhost");
+		config.put("port", 27017);
+		mongoClient = MongoClient.createShared(vertx, config);
+
+		EventBus eb = vertx.eventBus();
+		eb.consumer("db.get", handlerGet());
+		eb.consumer("db.put", handlerPut());
+	}
+
+	private Handler<Message<String>> handlerPut() {
+		return message -> {
+			String title = message.body();
+
+			JsonObject document = new JsonObject().put("title", title);
+
+			mongoClient.save("books", document, res -> {
+				JsonObject json = new JsonObject();
+				if (res.succeeded()) {
+					json.put("id", res.result()).put("result", "success");
+					message.reply(json);
+				} else {
+					json.put("result", "fail");
+					res.cause().printStackTrace();
+				}
+			});
+		};
+	}
+
+	private Handler<Message<JsonObject>> handlerGet() {
+		return message -> {
+			JsonObject query = new JsonObject();
+			mongoClient.find("books", query, res -> {
+				if (res.succeeded()) {
+					JsonObject js = new JsonObject();
+					js.put("list", res.result());
+					js.put("result", "success");
+					message.reply(js);
+				} else {
+					res.cause().printStackTrace();
+				}
+			});
+		};
+	}
+
+	// Optional - called when verticle is undeployed
+	public void stop() {
+	}
+
+}
--- vertx-web/src/main/java/vertx/web/sample/WebVerticle.java
+++ vertx-web/src/main/java/vertx/web/sample/WebVerticle.java
@@ -1,61 +1,70 @@
 package vertx.web.sample;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServer;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 /**
- * http://vertx.io/docs/vertx-web/java/
+ * vertx web, mongo-client, eventBus 사용한 예제
+ * 
+ * 메뉴얼 참조 http://vertx.io/docs/vertx-web/java/
+ * 
+ * 예제소스 참조
+ * https://github.com/vert-x3/vertx-examples/blob/master/web-examples/src/main/java/io/vertx/example/web/rest/SimpleREST.java
  * 
  * @author july
  *
  */
 public class WebVerticle extends AbstractVerticle {
 
+	EventBus eb;
+
 	@Override
 	public void start() {
-		HttpServer server = vertx.createHttpServer();
-
 		Router router = Router.router(vertx);
 		router.route().handler(BodyHandler.create());
-		//router.route().consumes("application/json").produces("application/json");
+		router.route().consumes("application/json").produces("application/json");
 
-		router.put("/from/to").handler(routingContext -> {
-			JsonObject pJo = routingContext.getBodyAsJson();
-			String from = pJo.getString("from");
-			String to = pJo.getString("to");
+		router.get("/book/title/:title").handler(this::handleGet);
+		router.put("/book/title").handler(this::handlePut);
 
-			JsonObject jo = new JsonObject();
-			jo.put("from", from);
-			jo.put("to", to);
-
-			HttpServerResponse response = routingContext.response();
-			response.putHeader("content-type", "application/json");
-			response.end(jo.encodePrettily());
-		});
-
-		router.get("/from/:from/to/:to").handler(routingContext -> {
-
-			String from = routingContext.request().getParam("from");
-			String to = routingContext.request().getParam("to");
-
-			JsonObject jo = new JsonObject();
-			jo.put("from", from);
-			jo.put("to", to);
-
-			HttpServerResponse response = routingContext.response();
-			response.putHeader("content-type", "application/json");
-			response.end(jo.encodePrettily());
-		});
-
-		server.requestHandler(router::accept).listen(8080);
+		eb = vertx.eventBus();
+		
+		vertx.deployVerticle("vertx.mongo.sample.MongodbpersisterVerticle");
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 	}
 
-	public void sstart() {
+	public void handleGet(RoutingContext routingContext) {
+		String title = routingContext.request().getParam("title");
+		eb.send("db.get", title, ar -> {
+			if (ar.succeeded()) {
+				response(routingContext, ar);
+			}
+		});
+	}
 
+	public void handlePut(RoutingContext routingContext) {
+		JsonObject pJo = routingContext.getBodyAsJson();
+		String title = pJo.getString("title");
+
+		eb.send("db.put", title, ar -> {
+			if (ar.succeeded()) {
+				response(routingContext, ar);
+			}
+		});
+	}
+	
+	private void response(RoutingContext routingContext, AsyncResult<Message<Object>> ar) {
+		System.out.println("Received reply: " + ar.result().body());
+		HttpServerResponse response = routingContext.response();
+		response.putHeader("content-type", "application/json");
+		response.end(((JsonObject) ar.result().body()).encodePrettily());
 	}
 
 }
